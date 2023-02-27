b8e15a9d11fe73c0b17caab0a30e20cf4c784a0d
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-02-27 15:51:39 +0530

    QA changes

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = (() => {
   const env = process.env.NODE_ENV || "development";
 
   // for security reason don't let to require any arbitrary file defined in process.env
-  if (["production", "development"].indexOf(env) < 0) {
+  if (["production", "development", "qa"].indexOf(env) < 0) {
     return require("./development");
   }
 
---
+++ config/qa.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /**
+   * Insert the URL of APIs, whenever they are ready.
+   */
+  TC_CHALLENGES_SERVICE_URL: "http://local.topcoder-qa.com:8504/api",
+
+  /**
+   * URL of Topcoder Community Website
+   */
+  TOPCODER_COMMUNITY_WEBSITE_URL: "https://topcoder-qa.com",
+
+  /**
+   * Email to report issues to
+   */
+  EMAIL_REPORT_ISSUE: "support+team-issue@topcoder-qa.com",
+};
